7da0c0116f631293786ded6244a399f8861e88aa
James Vodanovich <james@jvodan.com>
2015-01-22 09:49:44 +1100

    first commit

--- engines/scripts/post_install.sh
+++ engines/scripts/post_install.sh
@@ -1,3 +1,4 @@
-touch /home/app/install.lock 
-chmod go-w /home/app/dolibarr;
-cp -r /tmp/dolibarr-?.?.? /home/engines/local
+touch /home/app/install.lock ;
+/home.engines/scripts/revoke_rw_access.sh /home/app/dolibarr;
+cp -r /tmp/dolibarr-?.?.? /home/engines/local;
+touch /home/app/documents/install.lock ;
